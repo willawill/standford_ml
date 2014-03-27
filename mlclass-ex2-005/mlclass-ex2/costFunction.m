60da0a807cff3a58f1baac8db1cbef77c4d181ae
Willa <mowmowjoker@gmail.com>
2014-03-27 16:19:34 -0400

    70 so far.

--- mlclass-ex2-005/mlclass-ex2/costFunction.m
+++ mlclass-ex2-005/mlclass-ex2/costFunction.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+J = 1./m * (-y' * log(sigmoid(X * theta)) - (1 - y') * log(1 - sigmoid(X * theta)));
+grad = 1./m * X' * (sigmoid(X * theta) - y) ;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
